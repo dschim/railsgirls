5b334f2cb1a593dfd5dedf7b66d93a1698c0377e
daria <daria@schimke.ee>
2018-12-03 21:15:22 +0000

    colour change

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,8 @@ Rails.application.routes.draw do
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+Rails.application.routes.draw do
+  get 'pages/places'
+  root to: 'ideas#show'
+  resources :ideas
+end
